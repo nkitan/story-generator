dee1fc9298b2ee696ef2ba86889c926683084df0
nkitan <ankitdas2k@gmail.com>
2024-09-08 04:18:09 +0530

    Rename segments_id to segments in db schema

--- src/schemas/stories.ts
+++ src/schemas/stories.ts
@@ -8,7 +8,7 @@ export const stories = pgTable('stories',
   {
     id: serial('id').primaryKey(),
     input: varchar('input', { length: 1000 }).notNull(),
-    segments: serial('segment_id'),
+    segments: serial('segments'),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at').defaultNow().$onUpdateFn(() => new Date()).notNull(), // Automatically updated timestamp
     userId: integer('user_id').notNull().references(() => users.id) // Reference to the user who created the story
